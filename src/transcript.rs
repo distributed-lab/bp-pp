238055a07f5a93b070287033addf4839369bd6c9
olegfomenko <oleg.fomenko2002@gmail.com>
2024-05-16 16:22:22 +0300

    refactoring, minor changes

--- src/transcript.rs
+++ src/transcript.rs
@@ -3,12 +3,10 @@ use k256::elliptic_curve::group::GroupEncoding;
 use k256::elliptic_curve::PrimeField;
 use merlin::Transcript;
 
-#[inline]
 pub fn app_point(label: &'static [u8], p: &ProjectivePoint, t: &mut Transcript) {
     t.append_message(label, p.to_bytes().as_slice());
 }
 
-#[inline]
 pub fn get_challenge(label: &'static [u8], t: &mut Transcript) -> Scalar {
     let mut buf = [0u8; 32];
     t.challenge_bytes(label, &mut buf);
